a924f54f42eac3c390d66582064844be668f98cd
choijh97 <64878645+choijh97@users.noreply.github.com>
2020-05-06 14:37:07 +0900

    add mul,div,exit

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
   int num1,num2,option;
   printf("Open Source SW Project\n");
   printf("First Github Tranning\n");
-  printf("Choose \n1.add\n2.sub\n3.mul\n4/div\n5.exit\n:");
+  printf("Choose \n1.add\n2.sub\n3.mul\n4.div\n5.exit\n:");
   scanf("%d",&option);
   switch(option)
   {
@@ -23,6 +23,23 @@ int main()
       scanf("%d",&num2);
       printf("%d -%d =%d",num1,num2,num1-num2);
       break;
+    case 3:
+      printf("Enter the first number:");
+      scanf("%d",&num1);
+      printf("Enter the second number:");
+      scanf("%d",&num2);
+      printf("%d *%d =%d",num1,num2,num1*num2);
+      break;
+    case 4:
+      printf("Enter the first number:");
+      scanf("%d",&num1);
+      printf("Enter the second number:");
+      scanf("%d",&num2);
+      printf("%d /%d =%d",num1,num2,num1/num2);
+      break;
+    case 5:
+      printf("exit\n");
+     return 0;
     default:
       printf("You enter the wrong number\n");
       break;
