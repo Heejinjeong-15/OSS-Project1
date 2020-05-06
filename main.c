13c3c69737ca32e3bcfa06e50fb42ceb228ddb5f
정찬호 <54787512+jch1999@users.noreply.github.com>
2020-05-06 14:43:43 +0900

    remove exit part
    this code doesn't have repeat. so we decided to remove the 'exit' part.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
   int num1,num2,option;
   printf("Open Source SW Project\n");
   printf("First Github Tranning\n");
-  printf("Choose \n1.add\n2.sub\n3.mul\n4.div\n5.exit\n:");
+  printf("Choose \n1.add\n2.sub\n3.mul\n4.div\n:");
   scanf("%d",&option);
   switch(option)
   {
@@ -37,9 +37,6 @@ int main()
       scanf("%d",&num2);
       printf("%d /%d =%d",num1,num2,num1/num2);
       break;
-    case 5:
-      printf("exit\n");
-     return 0;
     default:
       printf("You enter the wrong number\n");
       break;
