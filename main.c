cd1b14338671a5c53c4c28e1a3f26c63ed3a0559
정찬호 <54787512+jch1999@users.noreply.github.com>
2020-05-06 11:17:51 +0900

    making the calculator's add
    I make the calculator's add part - 2018243087 정찬호

--- main.c
+++ main.c
@@ -2,8 +2,23 @@
 
 int main()
 {
+  int num1,num2,option;
   printf("Open Source SW Project\n");
   printf("First Github Tranning\n");
-  printf("2018243087 Jung ChanHo commit\n");
+  printf("Choose \n1.add\n2.sub\n3.mul\n4/div\n5.exit\n:");
+  scanf("%d",&option);
+  switch(option)
+  {
+    case 1: 1
+      printf("Enter the first number:");
+      scanf("%d",&num1);
+      printf("Enter the second number:");
+      scanf("%d",&num2);
+      printf("%d +%d =%d",num1,num2,num1+num2);
+      break;
+    default:
+      printf("You enter the wrong number\n");
+      breal;
+  }
   return 0;
 }
