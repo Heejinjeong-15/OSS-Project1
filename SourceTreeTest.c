2c29943e7b5e27970a42a393b74f8b3119eecb23
Heejinjeong-15 <higin150@gmail.com>
2020-05-20 10:46:15 +0900

    printf() 추가

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,9 +2,16 @@
 
 int main{}
 {
+int 1 = 0;
+
   printf("Fist OSS Code\n");
   printf("Second OSS Code\n");
 
+  printf("마스터 브렌치에 코드를 추가하였음");
+
+  for (int i -0;, i < 10;, i++){
+    printf("Test 브렌치\n");
+  }
 
   return 0;
 }
